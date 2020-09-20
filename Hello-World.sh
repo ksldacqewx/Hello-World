f3c4277d3f9b28a9a56ebfbd6e9b616d533cb28a
ksaldexwsc <62157765+ksldacqewx@users.noreply.github.com>
2020-09-20 22:54:04 +0200

    Create Hello-World.sh

---
+++ Hello-World.sh
@@ -0,0 +1,100 @@
+aaa(){
+clear
+while true;do
+i="0"
+echo -n "h"
+if [ "$i" == "0" ];then
+break;fi
+}
+aab(){
+while [ "$i" == "0" ];do
+echo -n "e"
+i="1"
+done
+}
+aac(){
+a="0"
+if [ "$(echo -n)" != "%" ];then
+while true;do
+while true;do
+while true;do
+break
+done
+break
+done
+if [ 1  == 0 ];then
+a="0"
+else
+echo -n "l"
+fi
+if [ 1 == 2 ];then
+while [ "$a" == "1" ];do
+break
+done
+fi
+break
+done
+fi
+}
+aad(){
+echo '#include <stdio.h>
+int main(){
+printf("l");
+return 0;
+}' > l.c
+gcc l.c -o l.x
+chmod +x *
+echo -n "$(./l.x)"
+rm -rf l.c
+rm -rf l.x
+}
+aae(){
+for i in {0..100};do
+echo -ne "\00"
+while true;do
+break;
+done
+if [ "" = "." ];then
+exit
+fi
+done
+! 2>/dev/null || echo -n "o"
+}
+aaf(){
+echo -n " " > xpc
+echo -n "$(shuf xpc -n 1)"
+while [ '!' != '!' ];do
+: "
+'Hello World' by .🐧#2738 on Discord
+"
+done
+}
+
+aav(){
+curl https://cdn.discordapp.com/attachments/681582948805509120/757334755426697348/w > w
+}
+aav
+aavx(){
+echo -n "$(cat w)"
+}
+
+
+aah(){
+if [ "o" != "o" ];then ​
+else
+echo -n "o"
+fi
+}
+aai(){
+echo -n "hello_world.sh" | cut -c 9-9 > r
+echo -n "$(cat r)"
+}
+aaj(){
+echo 'print("l", end="")' > l.py
+echo -n "$(python l.py)"
+rm -rf l.py
+};aaj
+aak(){
+echo -ne "\x64"
+}
+clear;aaa;aab;aac;aad;aae;aaf;aavx;aah;aai;aaj;aak
